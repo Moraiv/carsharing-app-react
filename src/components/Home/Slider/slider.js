97a29b22bd95fee999094668f0191b76f7980dd6
Moraiv <raivon.frost@gmail.com>
2021-07-22 14:36:38 +0400

    feat: added slider dots

--- src/components/Home/Slider/slider.js
+++ src/components/Home/Slider/slider.js
@@ -4,21 +4,21 @@ import leftArrow from '../../../assets/left-arrow.svg'
 import rightArrow from '../../../assets/right-arrow.svg'
 
 const Slider = () => {
-    const [step, setStep] = useState(1)
+    const [step, setStep] = useState(0)
     const [sliderClass, setSliderClass] = useState('__first')
 
     useEffect(() => {
         switch (step) {
-            case 1:
+            case 0:
                 setSliderClass('__first')
                 break
-            case 2:
+            case 1:
                 setSliderClass('__second')
                 break
-            case 3:
+            case 2:
                 setSliderClass('__third')
                 break
-            case 4:
+            case 3:
                 setSliderClass('__fourth')
                 break
             default:
@@ -27,25 +27,25 @@ const Slider = () => {
     }, [step])
 
     const stepLeft = () => {
-        if (step > 1) {
+        if (step > 0) {
             setStep(step - 1)
         } else {
-            setStep(4)
+            setStep(3)
         }
         setSliderClass('__first')
     }
 
     const stepRight = () => {
-        if (step < 4) {
+        if (step < 3) {
             setStep(step + 1)
         } else {
-            setStep(1)
+            setStep(0)
         }
     }
 
     const setSliderText = () => {
         switch (step) {
-            case 1:
+            case 0:
                 return (
                     <>
                         <h1 className='slider-text__title'>Бесплатная парковка</h1>
@@ -55,7 +55,7 @@ const Slider = () => {
                         </p>
                     </>
                 );
-            case 2:
+            case 1:
                 return (
                     <>
                         <h1 className='slider-text__title'>Страховка</h1>
@@ -64,7 +64,7 @@ const Slider = () => {
                         </p>
                     </>
                 );
-            case 3:
+            case 2:
                 return (
                     <>
                         <h1 className='slider-text__title'>Бензин</h1>
@@ -73,7 +73,7 @@ const Slider = () => {
                         </p>
                     </>
                 );
-            case 4:
+            case 3:
                 return (
                     <>
                         <h1 className='slider-text__title'>Обслуживание</h1>
@@ -95,6 +95,14 @@ const Slider = () => {
         }
     }
 
+    const renderDots = () => {
+        let dots = []
+            for(let i=0; i <= 3; i++) {
+            dots.push(<span className={`slider-dots__item slider-dots__item${(i === step) ? '_active' : ''}`}/>)
+            }
+        return dots
+    }
+
     return (
         <aside className={`slider-container slider-container${sliderClass}`}>
             <section className='slider-container__step-left' onClick={stepLeft}>
@@ -105,6 +113,9 @@ const Slider = () => {
                 <button className={`slider-text-button slider-text-button${sliderClass}`}>
                     <a className='slider-text__link'>Подробнее</a>
                 </button>
+            </section>
+            <section className='slider-dots'>
+                {renderDots().map((item) => {return item})}
             </section>
             <section className='slider-container__step-right' onClick={stepRight}>
                 <img className='slider-container__arrow' src={rightArrow} alt='arrow right'/>
